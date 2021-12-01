d7ee21b7accb2777c50352ef065f12038a1a4bb0
maksmerfy <wolfikitik@gmail.com>
2021-12-01 23:14:45 +0300

    Initial commit

---
+++ src/test/java/ru/mb/analytics/AnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mb.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
